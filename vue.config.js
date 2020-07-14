204466b469ef0cae1d9fabffa3407d4e0abe3bbb
Debabrata Sen <debabrata@klizos.com>
2020-07-15 02:07:07 +0530

    CHnages made

--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,32 @@
 module.exports = {
   publicPath: process.env.NODE_ENV === "production" ? "/nyt-app-pwa/" : "/",
+  pwa: {
+    name: "My App",
+    themeColor: "#4DBA87",
+    msTileColor: "#000000",
+    appleMobileWebAppCapable: "yes",
+    appleMobileWebAppStatusBarStyle: "black",
+
+    // configure the workbox plugin
+    workboxPluginMode: "InjectManifest",
+    workboxOptions: {
+      // swSrc is required in InjectManifest mode.
+      swSrc: "dev/sw.js",
+      // ...other Workbox options...
+      navigateFallback: "/index.html",
+      runtimeCaching: [
+        {
+          urlPattern: new RegExp("^https://api.nytimes.com/svc/"),
+          handler: "networkFirst",
+          options: {
+            networkTimeoutSeconds: 20,
+            cacheName: "api-cache",
+            cacheableResponse: {
+              statuses: [0, 200],
+            },
+          },
+        },
+      ],
+    },
+  },
 };
