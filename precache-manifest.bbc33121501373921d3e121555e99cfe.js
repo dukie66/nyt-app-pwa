90b4e0e5b0757f3b419c151e99479fd62bc96d06
Debabrata Sen <debabrata@klizos.com>
2020-07-15 01:44:49 +0530

    gh-pages

---
+++ precache-manifest.bbc33121501373921d3e121555e99cfe.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "02bb5e30019c60504bb7",
+    "url": "/nyt-app-pwa/css/app.74299d1d.css"
+  },
+  {
+    "revision": "ca8a2681e46afa6843df",
+    "url": "/nyt-app-pwa/css/chunk-vendors.cbc2a074.css"
+  },
+  {
+    "revision": "0f8410c1c69e3a82bc14195909bccfd1",
+    "url": "/nyt-app-pwa/index.html"
+  },
+  {
+    "revision": "02bb5e30019c60504bb7",
+    "url": "/nyt-app-pwa/js/app.f010bb72.js"
+  },
+  {
+    "revision": "ca8a2681e46afa6843df",
+    "url": "/nyt-app-pwa/js/chunk-vendors.3adf4ef0.js"
+  },
+  {
+    "revision": "52c0507b1cc17a713ed5dfe1371b0395",
+    "url": "/nyt-app-pwa/manifest.json"
+  },
+  {
+    "revision": "735ab4f94fbcd57074377afca324c813",
+    "url": "/nyt-app-pwa/robots.txt"
+  }
+]);
